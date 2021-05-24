ca84f25670252d7eeb31480a9957f25035002abf
Shiino Aya <26667561+dobby618@users.noreply.github.com>
2021-05-24 18:00:01 +0900

    finished 4章

--- dollar.rb
+++ dollar.rb
@@ -1,12 +1,18 @@
+# Value オブジェクト
 class Dollar
-  attr_reader :amount
-
   def initialize(amount)
     @amount = amount
   end
 
+  def equal?(other)
+    # amount を外部から参照するのはここしかないので
+    # amount を private にしておきたい
+    @amount == other.instance_eval('@amount')
+  end
+  alias_method :==, :equal?
+
   def times(multiplier)
     # value オブジェクトみたいにするってことか？
-    return Dollar.new(amount * multiplier)
+    return Dollar.new(@amount * multiplier)
   end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,10 +3,19 @@ require_relative '../dollar'
 describe 'MoneyText' do
   it 'testMultiplication' do
     five = Dollar.new(5)
-    product = five.times(2)
-    expect(product.amount).to eq 10
+    # times メソッドが doller オブジェクトを返してることが分かりづらい
+    # product = five.times(2)
+    # expect(product.amount).to eq 10
+    expect(five.times(2)).to eq Dollar.new(10)
 
-    product = five.times(3)
-    expect(product.amount).to eq 15
+    expect(five.times(3)).to eq Dollar.new(15)
+  end
+
+  it 'testEquality' do
+    expect(Dollar.new(5).equal?(Dollar.new(5))).to be_truthy
+    expect(Dollar.new(5) == Dollar.new(5)).to be_truthy
+    expect(Dollar.new(5)).to eq Dollar.new(5)
+
+    expect(Dollar.new(5)).not_to eq Dollar.new(6)
   end
 end
